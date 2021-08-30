2011fb4da2aff13f1bdbc05ade70a9413d54fc80
Brian David Lemke <blemke98@gmail.com>
2021-08-30 13:43:11 +0200

    first steps

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,14 @@
 
+
+print("Hallo und Wilkommen bei der IT Soulution")
+print("")
+print("")
+print("")
+print("")
+
+user_input = ""
+
+user_input = input()
+
+print("Hallo")
+print("Ihre Eingabe war:", user_input)
