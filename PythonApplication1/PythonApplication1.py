ab48a1b6d2eda7f4b61fd517ff0909229bf45e8b
Brian David Lemke <blemke98@gmail.com>
2021-08-30 14:21:41 +0200

    added classes

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,14 +1,21 @@
 
 
-print("Hallo und Wilkommen bei der IT Soulution")
-print("")
-print("")
-print("")
-print("")
+def F_MAIN():
+    F_INTRO()
 
-user_input = ""
 
-user_input = input()
+def F_INTRO():
+    print("Hallo und Wilkommen bei der IT Soulution")
+    print("Sie haben ein Problem, bei welchem Sie Hilfe benötigen?")
+    print("Bitte wählen Sie ein Stichwort aus, welches Ihr Problem am besten beschreibt.")
+    print("Folgende Themenbereiche stehen zur Auswahl:")
+    print("")
 
-print("Hallo")
-print("Ihre Eingabe war:", user_input)
+    user_input = ""
+
+    user_input = input()
+
+    print("Hallo")
+    print("Ihre Eingabe war:", user_input)
+
+F_MAIN()
