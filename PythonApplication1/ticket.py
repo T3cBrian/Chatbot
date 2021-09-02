fa5e045db350dd0e87497e93c7d661327619ff4a
T3cBrian <blemke98@gmail.com>
2021-09-02 10:38:47 +0000

    implemented ticket questions

--- PythonApplication1/ticket.py
+++ PythonApplication1/ticket.py
@@ -2,7 +2,7 @@ class ticket(object):
     """description of class ticket"""
 
 
-  def __init__(self, creatorname, mailadresse, telefonnumber, availability, category, description):
+def __init__(self, creatorname, mailadresse, telefonnumber, availability, category, description):
     self.creatorname = creatorname
     self.mailadresse = mailadresse
     self.telefonnumber = telefonnumber
